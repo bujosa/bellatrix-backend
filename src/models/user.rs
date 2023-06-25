c3c6a3058b6e28cf1de61a9b09c0441d20716a69
bujosa <davidbujosa@gmail.com>
2023-06-25 15:38:35 -0400

    refactor: move user into models

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,4 +1,16 @@
+use bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub username: String,
+    #[serde(rename = "profilePicture")]
+    pub profile_picture: Option<String>,
+    pub email: String,
+    pub password: String,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CreateUserDto {
