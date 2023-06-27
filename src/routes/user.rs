de36f29352293b9c0cb6694d9f4cf2787d081832
bujosa <davidbujosa@gmail.com>
2023-06-27 13:51:30 -0400

    fix(routes): update user routes with new changes related with web::data

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 
 use actix_web::{delete, get, post, put, web, HttpResponse, Responder, Scope};
 use bson::oid::ObjectId;
+use serde_json::json;
 
 use crate::{
     database::repositories::Repositories,
@@ -11,21 +12,23 @@ use crate::{
 
 #[post("")]
 async fn create_user(
-    user_repo: web::Data<UserRepository>,
+    data: web::Data<Repositories>,
     req: web::Json<CreateUserDto>,
 ) -> impl Responder {
     let create_user_dto: CreateUserDto = req.into_inner();
+    let user_repo: UserRepository = data.user_repository.clone();
     let user: Result<mongodb::results::InsertOneResult, mongodb::error::Error> =
         user_repo.create(create_user_dto).await;
 
     let id = user.unwrap().inserted_id.as_object_id().unwrap().to_hex();
 
-    HttpResponse::Created().json(id)
+    HttpResponse::Created().json(json!({ "id": id }))
 }
 
 #[get("/{id}")]
-async fn get_user(user_repo: web::Data<UserRepository>, id: web::Path<String>) -> impl Responder {
+async fn get_user(data: web::Data<Repositories>, id: web::Path<String>) -> impl Responder {
     let object_id = ObjectId::from_str(&id).unwrap();
+    let user_repo = data.user_repository.clone();
     let user = user_repo.get(object_id).await;
     match user {
         Some(user) => HttpResponse::Ok().json(user),
@@ -34,11 +37,9 @@ async fn get_user(user_repo: web::Data<UserRepository>, id: web::Path<String>) -
 }
 
 #[delete("/{id}")]
-async fn delete_user(
-    user_repo: web::Data<UserRepository>,
-    id: web::Path<String>,
-) -> impl Responder {
+async fn delete_user(data: web::Data<Repositories>, id: web::Path<String>) -> impl Responder {
     let object_id = ObjectId::from_str(&id).unwrap();
+    let user_repo = data.user_repository.clone();
     let deleted = user_repo.delete(object_id).await;
     if deleted {
         HttpResponse::NoContent().finish()
@@ -49,13 +50,15 @@ async fn delete_user(
 
 #[put("/{id}")]
 async fn update_user(
-    user_repo: web::Data<UserRepository>,
+    data: web::Data<Repositories>,
     id: web::Path<String>,
     req: web::Json<UpdateUserDto>,
 ) -> impl Responder {
     let object_id = ObjectId::from_str(&id).unwrap();
 
     let update_user_dto: UpdateUserDto = req.into_inner();
+
+    let user_repo = data.user_repository.clone();
 
     let updated = user_repo.update(object_id, update_user_dto).await;
 
@@ -68,11 +71,15 @@ async fn update_user(
 
 #[get("/{start}/{limit}")]
 async fn get_all_users(
-    user_repo: web::Data<UserRepository>,
+    data: web::Data<Repositories>,
     params: web::Path<(i64, i64)>,
 ) -> impl Responder {
     let (start, limit) = params.into_inner();
+
+    let user_repo = data.user_repository.clone();
+
     let users = user_repo.get_all(start, limit).await;
+
     HttpResponse::Ok().json(users)
 }
 
