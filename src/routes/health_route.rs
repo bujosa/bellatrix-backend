89bc77eadf62ee4bbe90a2db3e22fe77a398d2a1
bujosa <davidbujosa@gmail.com>
2023-06-27 21:34:05 -0400

    feat: add status in health and not found

--- src/routes/health_route.rs
+++ src/routes/health_route.rs
@@ -5,6 +5,7 @@ use crate::models::response::Response;
 #[get("")]
 pub async fn healthcheck() -> impl Responder {
     let response = Response {
+        status: "success".to_string(),
         message: "Everything is working fine".to_string(),
     };
     actix_web::HttpResponse::Ok().json(response)
--- src/routes/not_found.rs
+++ src/routes/not_found.rs
@@ -4,6 +4,7 @@ use crate::models::response::Response;
 
 pub async fn not_found() -> Result<HttpResponse> {
     let response = Response {
+        status: "error".to_string(),
         message: "Resource not found".to_string(),
     };
     Ok(HttpResponse::NotFound().json(response))
