224976a65b6c841d31e7c9e87b87ca91b3b741e8
bujosa <davidbujosa@gmail.com>
2023-06-29 03:19:15 -0400

    test(utils): add valid test for http builder function

--- src/utils/http_response_builder.rs
+++ src/utils/http_response_builder.rs
@@ -88,3 +88,60 @@ where
 pub fn build_no_content_http_response() -> HttpResponse {
     HttpResponse::NoContent().finish()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+    use actix_web::test;
+    use serde_json::json;
+
+    #[test]
+    async fn test_build_http_response() {
+        let response = build_http_response(json!({ "message": "Hello World" }));
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[test]
+    async fn test_build_bad_http_response() {
+        let response = build_bad_http_response(json!({ "message": "Hello World" }));
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    }
+
+    #[test]
+    async fn test_build_internal_server_error_http_response() {
+        let response =
+            build_internal_server_error_http_response(json!({ "message": "Hello World" }));
+        assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
+    }
+
+    #[test]
+    async fn test_build_not_found_http_response() {
+        let response = build_not_found_http_response(json!({ "message": "Hello World" }));
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[test]
+    async fn test_build_unauthorized_http_response() {
+        let response = build_unauthorized_http_response(json!({ "message": "Hello World" }));
+        assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+    }
+
+    #[test]
+    async fn test_build_forbidden_http_response() {
+        let response = build_forbidden_http_response(json!({ "message": "Hello World" }));
+        assert_eq!(response.status(), StatusCode::FORBIDDEN);
+    }
+
+    #[test]
+    async fn test_build_conflict_http_response() {
+        let response = build_conflict_http_response(json!({ "message": "Hello World" }));
+        assert_eq!(response.status(), StatusCode::CONFLICT);
+    }
+
+    #[test]
+    async fn test_build_no_content_http_response() {
+        let response = build_no_content_http_response();
+        assert_eq!(response.status(), StatusCode::NO_CONTENT);
+    }
+}
